e7b2e1b58922bf2a9ba341e87e3b83c7a0807e37
Mark Sarcevicz <smark@broadinstitute.org>
2020-03-17 15:56:49 -0400

    updated helm function

--- src/helmdeploy.sh
+++ src/helmdeploy.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 function helmdeploy {
   if [[ "${GOOGLE_APPLICATION_CREDENTIALS}" != "" ]] && [[ "${NAMESPACEINUSE}" != "" ]] && [[ "${gcr_tag}" != "" ]]; then
+    helm plugin install https://github.com/thomastaylor312/helm-namespace
+    helm repo add datarepo-helm https://broadinstitute.github.io/datarepo-helm
+    helm repo update
     helm namespace upgrade ${NAMESPACEINUSE}-secrets datarepo-helm/create-secret-manager-secret --version=${helm_secret_chart_version} \
       --install --namespace ${NAMESPACEINUSE} -f \
       "https://raw.githubusercontent.com/broadinstitute/datarepo-helm-definitions/master/integration/${NAMESPACEINUSE}/${NAMESPACEINUSE}Secrets.yaml"
